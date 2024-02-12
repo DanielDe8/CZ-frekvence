e9d6a4f259da9136a81d39e8ba603ea4c76e06c7
DanD222 <48011765+DanD222@users.noreply.github.com>
2024-02-12 23:11:24 +0100

    cz_frekvence.py - fix frequency format in json file

--- cz_frekvence.py
+++ cz_frekvence.py
@@ -4,10 +4,11 @@ import csv
 from openpyxl import load_workbook
 from bs4 import BeautifulSoup
 
-frequency_file = 'CZ_frekvence_2024_tst.xlsx'  # Read aerodrome codes to retrieve freqencies, and airport names and frequencies for json generation
+frequency_file = 'CZ_frekvence_2024.xlsx'  # Read aerodrome codes to retrieve freqencies, and airport names and frequencies for json generation
 
 
 def get_frequency(aerodrome):
+    """Retrieves frequency for aerodrome ICAO code from aim.rlp.cz website"""
     response = requests.get(f'https://aim.rlp.cz/vfrmanual/actual/{str(aerodrome).lower()}_text_cz.html')
     soup = BeautifulSoup(response.text, 'html.parser')
 
@@ -52,7 +53,12 @@ def get_frequency(aerodrome):
 
 
 def retrieve_frequencies(frequency_file):
-    """ Read airport ICAO code from column A. Look up airport frequency on aim.rlp.cz. Write frequency to column B. """
+    """
+    Read airport ICAO code from column A of Excel file.
+    Extract airport frequency from aim.rlp.cz website.
+    Write frequency to column B of Excel file.
+    Manual checking of retrieved frequencies required.
+    """
     print(frequency_file)
 
     wb = load_workbook(frequency_file)
@@ -77,8 +83,12 @@ def retrieve_frequencies(frequency_file):
 
 
 def create_json_file(frequency_file):
-    """Read airport name  """
+    """
+    Read airport name and frequency from Excel file.
+    Create a json file to be loaded into KRT2Bluetooth Android app.
+    """
     json_output_file = frequency_file.strip('.xlsx') + '.json'
+    json_data = []
 
     wb = load_workbook(frequency_file)
     ws = wb.active
@@ -88,22 +98,29 @@ def create_json_file(frequency_file):
 
     try:
         with open(json_output_file, 'w') as file:
+
             for i in range(1, max_row + 1):
                 airport = ws['C' + str(i)].value  # read airport name from column C
                 frequency = ws['B' + str(i)].value  # read airport frequency from column B
+                # frequency = "{:.3f}".format(float(frequency))  # add trailing zeroes to frequency
 
-                json_data = json.dumps({"idx": i-1, "label": airport, "freq": frequency})
-                print(json_data)
+                print(airport, frequency)
 
-                file.write(json_data)
-                file.write('\n')
+                json_data.append({"idx": i - 1, "label": airport, "freq": float(frequency)})
+
+            all_json_data = {"name": frequency_file.replace('_frekvence', '_freq').strip('.xlsx'), "items": json_data}
+            file.write(json.dumps(all_json_data))
+
         print("Data added to JSON file successfully.")
     except Exception as e:
         print(f"An error occurred: {e}")
 
 
 def create_csv_file(frequency_file):
-    """Read airport name  """
+    """
+    Read airport name and frequency from Excel file.
+    Create a csv file to be loaded into KRT2 Manager PC app.
+    """
     csv_output_file = frequency_file.strip('.xlsx') + '.csv'
 
     wb = load_workbook(frequency_file)
@@ -118,6 +135,7 @@ def create_csv_file(frequency_file):
             for i in range(1, max_row + 1):
                 airport = ws['C' + str(i)].value  # read airport name from column C
                 frequency = ws['B' + str(i)].value  # read airport frequency from column B
+                # frequency = "{:.3f}".format(float(frequency))  # add trailing zeroes to frequency
 
                 csv_data = [airport, frequency]
                 print(csv_data)
@@ -130,8 +148,6 @@ def create_csv_file(frequency_file):
 
 if __name__ == "__main__":
     retrieve_frequencies(frequency_file)  # Read aerodrome ICAO codes from Excel file and retrieve frequencies from web, write to the same file
-
     create_json_file(frequency_file)  # Read airport names and frequencies from frequency Excel file, generate new json file
-
     create_csv_file(frequency_file)  # Read airport names and frequencies from frequency Excel file, generate new csv file
 
